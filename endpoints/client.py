d319f2151bc9f49dbb95c8efbd7a5899bc27110f
DMcDonald57 <d.mcdonald579@gmail.com>
2023-02-22 21:08:19 -0700

    Client and restaurant login logout works without token portion

--- endpoints/client.py
+++ endpoints/client.py
@@ -6,13 +6,21 @@ import uuid
 
 # endpoints for clients go here
 
-@app.post('/api/foodiepart2')
+@app.post('/api/clientsession')
 def client_login():
     email = request.json.get('email')
     password = request.json.get('password')
     result = run_statement('CALL client_login (?,?)' , [email, password])
-    # uuidOne = uuid.uuid1()
     if (type(result) == list):
-        return json.dumps(result, default=str)
+        return json.dumps("clientId: {}".format (result, default=str))
     else:
-        return "There was an error"
+        return make_response(jsonify(result), 500)
+
+@app.delete('/api/clientsession')
+def client_logout():
+    id = request.json.get('id')
+    result = run_statement('CALL client_logout (?)', [id])
+    if result == None:
+        return make_response(jsonify("Client Logged out"), 200)
+    else:
+        return make_response(jsonify("Something went wrong"), 500)
--- endpoints/restaurant.py
+++ endpoints/restaurant.py
@@ -1,5 +1,25 @@
 from app import app
 from flask import request, make_response,jsonify
 from dbhelpers import run_statement
+import json
 
 # endpoints for restaurants go here
+
+@app.post('/api/restaurantsession')
+def restaurant_login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    result = run_statement('CALL Restaurant_login (?,?)' , [email, password])
+    if (type(result) == list):
+        return json.dumps("restaurantId: {}".format (result, default=str))
+    else:
+        return make_response(jsonify(result), 500)
+
+@app.delete('/api/restaurantsession')
+def restaurant_logout():
+    id = request.json.get('id')
+    result = run_statement('CALL restaurant_logout (?)', [id])
+    if result == None:
+        return make_response(jsonify("Restaurant Logged out"), 200)
+    else:
+        return make_response(jsonify("Something went wrong"), 500)
