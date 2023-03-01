4e2f2215b1924db61dc860b5f2aedf38e50bac6a
DMcDonald57 <d.mcdonald579@gmail.com>
2023-02-28 20:50:25 -0700

    All menu end points are working as intended

--- endpoints/menu.py
+++ endpoints/menu.py
@@ -38,3 +38,45 @@ def add_menu_item():
         return make_response(jsonify("Restaurant info updated"), 200)
     else:
         return make_response(jsonify("Something went wrong"), 500)
+    
+@app.patch('/api/menu')
+def update_menu_item():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    token = uuid.uuid4().hex
+    menu_id = request.json.get('menu_id')
+    name = request.json.get('name')
+    discription = request.json.get('discription')
+    price = request.json.get('price')
+    img_url = request.json.get('img_url')
+    result = run_statement("CALL verify_restaurant (?)", [email])
+    restaurant_id=result[0][0]
+    storedpassword=result[0][1]
+    if token != token:
+        return make_response((jsonify("Error"),401))
+    if storedpassword != password:
+        return make_response(jsonify("Password does not match"), 401)
+    result = run_statement('CALL update_menu_item (?,?,?,?,?,?)', [menu_id, name, discription, price, img_url, restaurant_id])
+    if result == None:
+        return make_response(jsonify("Restaurant info updated"), 200)
+    else:
+        return make_response(jsonify("Something went wrong"), 500)
+    
+@app.delete('/api/menu')
+def delete_menu_item():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    token = uuid.uuid4().hex
+    menu_id = request.json.get('menu_id')
+    result = run_statement("CALL verify_restaurant (?)", [email])
+    restaurant_id=result[0][0]
+    storedpassword=result[0][1]
+    if token != token:
+        return make_response((jsonify("Error"),401))
+    if storedpassword != password:
+        return make_response(jsonify("Password does not match"), 401)
+    result = run_statement('CALL delete_menu_item (?,?)', [menu_id, restaurant_id])
+    if result == None:
+        return make_response(jsonify("Menu Item has been deleted"), 200)
+    else:
+        return make_response(jsonify("Something went wrong"), 500)
